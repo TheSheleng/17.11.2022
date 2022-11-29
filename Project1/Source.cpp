17173bffd57de88304b6793ce00c2ebd1f4b2990
The_Sheleng <sheleng55@gmail.com>
2022-11-29 09:47:28 +0200

    1

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,7 +2,10 @@
 #include <tchar.h>
 #include <tlhelp32.h>
 #include <stdio.h>
+#include <ctime>
 #include "resource.h"
+
+#define DEFFAULT_STR_SIZE 256
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -12,6 +15,7 @@ HWND hNew;
 HWND hProcName;
 HWND hProcList;
 HWND hStatID;
+HWND hTime;
 
 int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpszCmdLine, int nCmdShow)
 {
@@ -45,6 +49,24 @@ void ShowProcList(HWND hList) {
 	CloseHandle(hSnapshot);
 }
 
+DWORD WINAPI Thread(LPVOID lp)
+{
+	WCHAR buff[DEFFAULT_STR_SIZE]{};
+
+	while (true)
+	{
+		ShowProcList(hProcList);
+
+		_wstrtime_s(buff, DEFFAULT_STR_SIZE);
+		wcscat_s(buff, DEFFAULT_STR_SIZE, TEXT(" "));
+		_wstrdate_s(buff + wcslen(buff), DEFFAULT_STR_SIZE);
+
+
+		SetWindowText(hTime, buff);
+
+		Sleep(5000);
+	}
+}	
 BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 {
 	switch (mes)
@@ -57,7 +79,9 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 		hProcName = GetDlgItem(hWnd, IDC_EDIT1);
 		hProcList = GetDlgItem(hWnd, IDC_LIST1);
 		hStatID = GetDlgItem(hWnd, IDC_EDIT2);
-	   	ShowProcList(hProcList);
+		hTime = GetDlgItem(hWnd, IDC_OUTTIME);
+
+		CreateThread(NULL, 0, Thread, hWnd, 0, NULL);
 	}
 	return TRUE;
 
@@ -71,7 +95,6 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 
 			if (file != NULL)
 			{
-				const unsigned DEFFAULT_STR_SIZE = 256;
 				WCHAR buff[DEFFAULT_STR_SIZE]{};
 
 				const int COUNT = SendMessage(hProcList, LB_GETCOUNT, 0, 0);
@@ -100,8 +123,8 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 		}
 		else if (LOWORD(wp) == IDC_BUTTON3) 
 		{
-			wchar_t buf[260]{ 0 };
-			GetWindowText(hProcName, buf, 260);
+			wchar_t buf[DEFFAULT_STR_SIZE]{ 0 };
+			GetWindowText(hProcName, buf, DEFFAULT_STR_SIZE);
 			STARTUPINFO info = { sizeof(info) };
 			PROCESS_INFORMATION processInfo;
 			if (CreateProcess(buf, buf, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo))
@@ -114,7 +137,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT mes, WPARAM wp, LPARAM lp)
 			EnableWindow(hTerminate, TRUE);
 			LRESULT index = SendMessage(hProcList, LB_GETCURSEL, 0, 0);
 			int procId = SendMessage(hProcList, LB_GETITEMDATA, WPARAM(index), 0);
-			TCHAR buf[30];
+			TCHAR buf[DEFFAULT_STR_SIZE];
 			wsprintf(buf, TEXT("%i"), procId);
 			SendMessage(hStatID, WM_SETTEXT, 0, LPARAM(buf));			
 		}
